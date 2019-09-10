68f66dfa305c8144fae9c95741365876894adf91
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2019-09-10 10:50:05 -0700

    Added Loop Notes
    9.10.19

--- Notes/Loops.java
+++ Notes/Loops.java
@@ -5,8 +5,38 @@
  */
 public class Loops
 {
-    
     public static void main(){
-        System.out.println("We did a thing!");
+        for (int i=0; i<100; i++){
+            System.out.println("We did a thing!");
+        }
+    }
+    public static void main2(){
+        for (int i=0; i<100; i+=2){
+            System.out.println("We did a thing!");
+        }
+    }
+    public static void main3(){
+        for (int i=0; i<10; i+=2){
+            System.out.println(i);
+        }
+    }
+    public static void main4(){
+        for (int i=0; i<10;){
+            System.out.println(i);
+            i++;
+        }
+    }
+    public static void main5(){
+        for (int i=10; i>0; i--){
+            System.out.println(i);
+        }
+    }
+    public static void main6(){
+        for (int i=0; i<5; i++){
+            for (int j=0; j<6; j++){
+            System.out.print(j+" ");
+            }
+            System.out.println();
+        }    
     }
 }
