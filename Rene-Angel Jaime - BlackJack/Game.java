b5a7a210c782a762aaf7907035067c5f38cdc6e3
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2019-09-24 15:43:47 -0700

    afterschool work and help
    8.24.19

--- Rene-Angel Jaime - BlackJack/Game.java
+++ Rene-Angel Jaime - BlackJack/Game.java
@@ -12,32 +12,34 @@ public class Game
     static Hand player = new Hand();
     static Hand dealer = new Hand();
     public static void main(String[] args){
-        System.out.println("Welcome to BlackJack!");
-        System.out.println("The Dealer shows their first card, "+dealer.card1+", and keeps their second card hidden.");
+        System.out.println("Welcome to BlackJack!"); //Starting Introduction
+        System.out.println("The Dealer draws 2 cards the shows their first card, "+dealer.getCard1()+", and keeps their second card hidden."); //Stating dealer;s
+        System.out.println("You draw two cards, "+player.getCard1+"and"+player+"Your hand total is "+player.getTotal()+".");
         System.out.println();
         boolean playerTurn = true;
         boolean dealerTurn = true;
         while (playerTurn = true){
-            System.out.println("Your hand total is "+player.total+".");
             System.out.println("Hit or Stand?: ");
             String playerInput = input.nextLine();
             System.out.println();
             if (playerInput.equals("hit")){
                 player.hit();
-                System.out.println("You drew a "+player.hitCard+".");
+                System.out.println("You drew a "+player.getHitCard()+".");
+                System.out.println("Your hand total is "+player.getTotal()+".");
             } else if (playerInput.equals("stand")){
                 System.out.println("You stand, and the Dealer plays.");
                 System.out.println();
-                playerTurn = true;
+                playerTurn = false;
             }
         }
+        System.out.println("Dealer reveals their card, "+dealer.getCard2()+", the total is "+dealer.getTotal()+".");
         while (dealerTurn = true){
-            System.out.println("Dealer reveals their card, "+dealer.card2+", the total is "+dealer.total+".");
             dealer.hit();
-            System.out.println("Dealer drew a "+dealer.hitCard+", and the total is now "+dealer.total+".");
-            if (dealer.total >= 17){
+            System.out.println("Dealer drew a "+dealer.getHitCard()+", and the total is now "+dealer.getTotal()+".");
+            if (dealer.getTotal() >= 17){
                 dealerTurn = false;
             }
         }
+        
     }
 }
--- Rene-Angel Jaime - BlackJack/Hand.java
+++ Rene-Angel Jaime - BlackJack/Hand.java
@@ -7,10 +7,11 @@ import java.lang.Math;
 
 public class Hand
 {
-    public int card1;
-    public int card2;
-    public int total;
-    public int hitCard;
+    private int card1;
+    private int card2;
+    private int total = 0;
+    private int hitCard;
+    private boolean bust;
     public Hand(){
         card1 += (int)(Math.random() *11) +1;
         card2 += (int)(Math.random() *11) +1;
@@ -19,6 +20,25 @@ public class Hand
     public int hit(){
         hitCard = (int)(Math.random() *11) +1;
         total += hitCard;
+        return total;
+    }
+    public boolean bust(){
+        if (total > 21){
+            bust = true;
+        }
+        return bust;
+    }
+    //Accessor Methods
+    public int getCard1(){
+        return card1;
+    }
+    public int getCard2(){
+        return card2;
+    }
+    public int getHitCard(){
+        return hitCard;
+    }
+    public int getTotal(){
         return total;
     }
 }
