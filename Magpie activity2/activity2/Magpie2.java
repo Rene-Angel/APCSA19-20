e0eb097fdd61aa8c528feb252793667c29a25c4c
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2019-10-02 10:50:08 -0700

    MAGPIE!!!!!!
    10.2.19

--- Magpie activity2/activity2/Magpie2.java
+++ Magpie activity2/activity2/Magpie2.java
@@ -11,37 +11,44 @@ public class Magpie2
     public String getResponse(String statement)
     {
         String response = "";
-        statement = statement.trim();
-        if (statement.indexOf("no") >= 0)
+        if (statement.trim().indexOf("mother") >= 0
+                || statement.trim().indexOf("father") >= 0
+                || statement.trim().indexOf("sister") >= 0
+                || statement.trim().indexOf("brother") >= 0)
+        {
+            response = "Tell me more about your family.";
+        }
+        else if (statement.trim().indexOf("Mr.Wiebe") >= 0
+                || statement.trim().indexOf("mr.wiebe") >= 0
+                || statement.trim().indexOf("Wiebe") >= 0
+                || statement.trim().indexOf("wiebe") >= 0)
+        {
+            response = "He sounds like a very cool teacher.";
+        }
+        else if (statement.trim().indexOf("dog") >= 0
+                || statement.trim().indexOf("cat") >= 0)
+        {
+            response = "Tell me more about your pets.";
+        }
+        else if (statement.trim().indexOf("hmm") >= 0)
+        {
+            response = "Yes?";
+        }
+        else if (statement.trim().indexOf("hello") >= 0)
+        {
+            response = "Have we met?";
+        }
+        else if (statement.trim().indexOf("yes") >= 0)
+        {
+            response = "Yes, what?";
+        }
+        else if (statement.trim().indexOf("no") >= 0)
         {
             response = "Why so negative?";
         }
         else if (statement.indexOf(" ") >= 0)
         {
             response = "Say something, please.";
-        }
-        else if (statement.indexOf("Mr.Wiebe") >= 0
-                || statement.indexOf("mr.wiebe") >= 0
-                || statement.indexOf("Wiebe") >= 0
-                || statement.indexOf("wiebe") >= 0)
-        {
-            response = "He sounds like a very cool teacher.";
-        }
-        else if (statement.indexOf("dog") >= 0
-                || statement.indexOf("cat") >= 0)
-        {
-            response = "Tell me more about your pets.";
-        }
-        else if (statement.indexOf("hmm") >= 0)
-        {
-            response = "Yes?";
-        }
-        else if (statement.indexOf("mother") >= 0
-                || statement.indexOf("father") >= 0
-                || statement.indexOf("sister") >= 0
-                || statement.indexOf("brother") >= 0)
-        {
-            response = "Tell me more about your family.";
         }
         else
         {
@@ -55,6 +62,17 @@ public class Magpie2
         double r = Math.random();
         int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
         String response = "";
+        
+        /*
+         * Question: What happens when a keyword is included in another word?
+         *           Consider statements like "I know all the state capitals" and "I like
+         *           vegetables smothered in cheese." Explain the problem with the responses
+         *           to these statements.
+         *           
+         * Response: When a keyword is included in another word, the responses
+         * 
+         * 
+         */
         
         if (whichResponse == 0)
         {
