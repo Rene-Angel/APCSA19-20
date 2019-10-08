f03af6748838983f98ecec39f0a727f3cff4dbd1
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2019-10-08 10:49:33 -0700

    I dont like magpie
    10.8.19

---
+++ Rene-Angel Jaime - Magpie Activity3/Questions.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class Questions here.
+ *
+ * @author (Rene-Angel Jaime)
+ * @version (10.8.19)
+ */
+// iteration - 1, psn - 1, before - " ", after - nothing
+// iteration - 1, psn - 0, before - nothing, after - " "
+// iteration 
