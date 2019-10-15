0f8d993e0886f7033678cb2b6bf3e487f45dd548
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2019-10-15 10:50:37 -0700

    Array Lessosn and Notes
    10.15.19

---
+++ Arrays/Arrays.java
@@ -0,0 +1,33 @@
+/**
+ * @author Rene-Angel Jaime
+ * @version (10.15.19)
+ */
+import java.util.Scanner;
+public class Arrays
+{
+    int[] newArray = new int[100];
+    Scanner[] scannerArray = new Scanner[40];
+    // This only works if you Declare AND INITIALIZE on the same line
+    int[] newArray2 = {5,3,6,8,45,21,7};
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+    public Arrays()
+    {
+        newArray[0] = 5;
+        newArray[1] = 9;
+        newArray[2] = 7;
+        newArray[3] = 8;
+    }
+    public void changeNums()
+    {
+        newArray2[5] = 9001;
+    }
+    public void printArray()
+    {
+        for(int i = 0; i < newArray2.length; i++){
+            System.out.println(newArray2[i]);
+        }
+    }
+}
