841282b7e9c8888b6291c3cabf510be1f4f8d95d
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2019-10-22 10:50:48 -0700

    Help was gained today
    10.22.19

--- apcsReplaceAll/StringUtil.java
+++ apcsReplaceAll/StringUtil.java
@@ -5,37 +5,33 @@
  */
 public class StringUtil
 {
-    public static String apcsReplaceAll(String str, String oldStr, String newStr)
-    {
-        // (a)
-        str = " ";
-        oldStr = " ";
-        newStr = " ";
-    }
-    
-    /* public static String replaceNameNickname(String str, List<Person> people)
-    {
-        // (b)
-    }
-    
     public static void main()
     {
-        /*
-         * System.out.println("This should say '2 be or not 2 be'");
-         * System.out.println(apcsReplaceAll("to be or not to be","to","2"));
-         * System.out.println();
-         * 
-         * System.out.println("This should say 'advanced calculus'");
-         * System.out.println(apcsReplaceAll("advanced calculus","math","science"));
-         * System.out.println();
-         * 
-         * System.out.println("This should say 'gonegonegone'");
-         * System.out.println(apcsReplaceAll("gogogo","go","gone"));
-         * System.out.println();
-         * 
-         * System.out.println("This should say 'baa'");
-         * System.out.println(apcsReplaceAll("aaaaa","aaa","b"));
-         * System.out.println();
-         */
-    // }
+       System.out.println("This should say '2 be or not 2 be'");
+       System.out.println(apcsReplaceAll("to be or not to be","to","2"));
+       System.out.println();
+       
+       System.out.println("This should say 'advanced calculus'");
+       System.out.println(apcsReplaceAll("advanced calculus","math","science"));
+       System.out.println();
+       
+       System.out.println("This should say 'gonegonegone'");
+       System.out.println(apcsReplaceAll("gogogo","go","gone"));
+       System.out.println();
+       
+       System.out.println("This should say 'baa'");
+       System.out.println(apcsReplaceAll("aaaaa","aaa","b"));
+       System.out.println();     
+    }
+    public static String apcsReplaceAll(String str, String oldStr, String newStr)
+    {
+       String newString = str;  //Make a copy of the string (Avoids a y penalty)
+       // Find the position (psn) of the first oldStr
+       int psn = str.indexOf(oldStr);
+       // Create a loop that keeps going until we no longer have to replace stuff
+       while (psn != -1){
+           return newStr;
+       }
+       return str;
+    }
 }
