0526129d859ca154585df1d57bb39ce388ab0096
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2020-01-07 10:51:13 -0800

    2DArrays
    1.7.20

---
+++ 2DArrays/TwoDArrays.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class TwoDArrays here.
+ *
+ * @author Rene-Angel Jaime
+ * @version (1.7.20)
+ */
+public class TwoDArrays
+{
+    int[] array = new int[10];
+    int[][] tdArray = new int[10][10];
+    int[][][][][][][][][][] tenD;
+    //picture = 2D array of pixels (3D)
+    //px = [r,g,b]
+    //folder = array of pictures (4D)
+    //deal with multiple dimensions
+    
+    //row major formatl: row comes first and columns go second
+    //int[][] arr = int[4][4]
+    //arr[2][2]
+    //arr[3][1]
+    //arr[2][3]
+    public TwoDArrays(){
+        tdArray[0][0] = 1;
+        tdArray[1][3] = 4;
+    }
+    public void numberAllCells(){
+        int num = 1;
+        for(int i = 0; i < tdArray.length; i++){
+            for(int j = 0; j < tdArray.length; j++){
+                
+            }
+        }
+    }
+}
