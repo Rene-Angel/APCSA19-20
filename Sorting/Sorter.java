4a031bbf7452bd1491b4b31b89e09d4cf3d7f126
Rene-Angel <31977772+Rene-Angel@users.noreply.github.com>
2020-03-11 10:50:20 -0700

    3.11.20
    I have absolute no idea what the heck I am doing in this class anymore. This is such a HUGE OOF XD.

---
+++ Sorting/Sorter.java
@@ -0,0 +1,20 @@
+/**
+ * @author Rene-Angel Jaime
+ * @version (3.11.20)
+ */
+import java.util.ArrayList;
+import java.util.List;
+public class Sorter
+{
+    private int times;
+    public static int[] method1(int[] array, int length){
+        for(int i = 0; i < array.length; i++){
+            if(i< i++){
+                
+            }
+            times++;
+        }
+        return times;
+        return array;
+    }
+}
